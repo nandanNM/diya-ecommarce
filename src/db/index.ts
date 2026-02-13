8a9aa9adce4b737e85233be491416c09696f831f
CoderNandan <nandanindia1947@gmail.com>
2026-02-13 09:27:24 +0530

    feat: update database schema configuration to include additional schema file

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 import * as authSchema from "./auth-schema";
+import * as schema from "./schema";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -12,7 +13,7 @@ export const pool = new Pool({
 });
 
 export const db = drizzle(pool, {
-  schema: { ...authSchema },
+  schema: { ...schema, ...authSchema },
   logger: true,
 });
 
